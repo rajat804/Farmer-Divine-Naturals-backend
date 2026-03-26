0d316c6b812505a5accd8dcbc15fb4c01e18a197
Rajat <rajat804@gmail.com>
2026-03-26 15:58:06 +0530

    server update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import session from "express-session"
 // import dotenv from "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import adminRoutes from "./routes/adminRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 // Routes
 import { promises as dns } from "dns";
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
@@ -50,7 +51,8 @@ app.use(cors(corsOptions));
    MIDDLEWARE
 ============================== */
 app.use(express.json());
-
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 /* ==============================
    ROUTES
@@ -65,8 +67,9 @@ app.use(
     }
   })
 );
-app.use("/api/admin", adminRoutes)
 
+app.use("/api/admin", adminRoutes)
+app.use("/api/products", productRoutes)
 
 /* ==============================
    TEST ROUTE
